6aa9b005bc05e245bc09cf53ded3ddac61fe5c9f
cxreli <arellitrevino@gmail.com>
2018-08-08 12:46:17 -0500

    Initial Commit

---
+++ platforms/browser/www/respuestas.php
@@ -0,0 +1,26 @@
+//respuestas correctas
+				  /*r1=POST[respuesta3];
+					r2=POST[respuesta2];
+					r3=POST[respuesta1];
+
+					r4=POST[respuesta2];
+					r5=POST[respuesta2];
+					r6=POST[respuesta3];
+
+					r7=POST[respuesta1];
+					r8=POST[respuesta1];
+					r9=POST[respuesta1];
+					r10=POST[respuesta3]; 
+
+					r1=data[0].respuesta3;
+					r2=data[1].respuesta2;
+					r3=data[2].respuesta1;
+
+					r4=data[3].respuesta2;
+					r5=data[4].respuesta2;
+					r6=data[5].respuesta3;
+
+					r7=data[6].respuesta1;
+					r8=data[7].respuesta1;
+					r9=data[8].respuesta1;
+					r10=data[9].respuesta3;*/
